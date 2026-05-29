084c90e748c2d371b17aa8cfef1f420dbcb48781
hawk1411 <ivargxp@gmail.com>
2026-05-29 16:54:41 +0530

    form 2 capturing response,display of success and warning message

--- lecture1.py
+++ lecture1.py
@@ -2,18 +2,29 @@ import streamlit as st
 import time
 
 #initial header with markdown
-st.markdown("<h1>User Registration<h2>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>User Registration</h1>", unsafe_allow_html=True)
 #creating a form object
 form =  st.form("form1")
 form.text_input("FIRST NAME")
 
 form.form_submit_button("submit")
 
-with st.form("form2"):
-    col1, col2 = st.columns(2)
-    col1.text_input("demonstration1")
-    col2.text_input("demonstration2")
-    st.text_input("First_name")
-    st.checkbox("hey")
-    st.form_submit_button("submit")
-
+with st.form("form2", clear_on_submit=True):
+    day, month, year = st.columns(3)
+    
+    c = day.text_input("DAY")
+    a = month.text_input("MONTH")
+    b = year.text_input("year")
+    
+    sub = st.form_submit_button("submit")
+    if sub :
+        if c =="" or a == "" or b == "" :
+            st.warning("please fil the empty widgets")
+    
+        else: st.success("from submitted") 
+    #     st.write("please fill and submit the form")
+    # else:
+    #         st.write(a)
+    #         st.write(b)
+    #         st.write(c)
+    
